0c17f67dd92a2d96df5daa06ac753b21d952988c
Vidrul <Vidrul@ugithub.com>
2020-02-20 16:14:29 +0300

    Initial Commit

---
+++ home12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  home12
+//
+//  Created by David Saley on 2/20/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
